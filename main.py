04979472d2162f864e9884dbe3e44d004d8dd4ac
Alexey Demidov <ademido@gmail.com>
2023-04-28 20:30:23 +0000

    Check 2nd build

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CD and Now on GCP it is Continues Delivery'
+    return 'Hello World! CD and Now on GCP it is Continues Delivery, 2nd trigger'
 
 @app.route('/echo/<name>')
 def echo(name):
