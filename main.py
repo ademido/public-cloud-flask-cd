e408d2e09bc100054885dbc21f825a2a87140f73
Alexey <131382109+ademido@users.noreply.github.com>
2023-04-28 23:04:08 +0300

    Update main.py
    Updated text for a change

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return 'Hello World! CD'
+    return 'Hello World! CD and Now on GCP it is Continues Delivery'
 
 @app.route('/echo/<name>')
 def echo(name):
